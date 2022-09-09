7bed9c44ca126af308e6a8c907f996586bccf874
imgaram <s21049@gsm.hs.kr>
2022-09-09 21:02:47 +0900

    회원가입 이메일 인증 화면 디자인

---
+++ app/src/main/java/com/example/job_gsm/view/user/EmailCertificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.job_gsm.view.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.job_gsm.R
+
+class EmailCertificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_certification)
+    }
+}
