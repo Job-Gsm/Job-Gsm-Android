eeb8be5c8df69230e7dbbe5731a6a26ded89e39d
imgaram <s21049@gsm.hs.kr>
2022-09-18 17:58:01 +0900

    메인 화면 popup menu 추가

--- app/src/main/java/com/example/job_gsm/view/MainActivity.kt
+++ app/src/main/java/com/example/job_gsm/view/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.job_gsm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
 import com.example.job_gsm.R
 import com.example.job_gsm.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,5 +16,34 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.menuImage.setOnClickListener {
+            setPopMenu(it)
+        }
+    }
+
+    private fun setPopMenu(view: View) {
+        val popupMenu = PopupMenu(applicationContext, view)
+        menuInflater.inflate(R.menu.main_context_menu, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.profile -> {
+                    Toast.makeText(this@MainActivity, "profile", Toast.LENGTH_SHORT).show()
+                }
+                R.id.notification -> {
+                    Toast.makeText(this@MainActivity, "notification", Toast.LENGTH_SHORT).show()
+                }
+                R.id.option -> {
+                    Toast.makeText(this@MainActivity, "option", Toast.LENGTH_SHORT).show()
+                }
+                R.id.bookmark_list -> {
+                    Toast.makeText(this@MainActivity, "bookmark_list", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            false
+        }
+        popupMenu.show()
     }
 }
