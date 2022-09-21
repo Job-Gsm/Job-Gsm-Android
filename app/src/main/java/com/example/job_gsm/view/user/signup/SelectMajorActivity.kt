eba614d6d515badaf37b705817dfc71cb06d6f68
imgaram <s21049@gsm.hs.kr>
2022-09-21 20:38:15 +0900

    전공 선택 화면 생성, 디자인

---
+++ app/src/main/java/com/example/job_gsm/view/user/signup/SelectMajorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.job_gsm.view.user.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.job_gsm.R
+
+class SelectMajorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_major)
+    }
+}
