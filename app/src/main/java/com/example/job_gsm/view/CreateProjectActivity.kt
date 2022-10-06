4df5acbd85a6f825e9571174be14a3997acb8124
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-06 20:12:39 +0900

    프로젝트 생성 퍼블리싱

---
+++ app/src/main/java/com/example/job_gsm/view/CreateProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.example.job_gsm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.job_gsm.R
+
+class CreateProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_project)
+    }
+}
